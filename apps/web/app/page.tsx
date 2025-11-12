ca1c15a05889b5e2c5fc3b5ac06adb51209d7231
Nishanth Murugan <nishanth.murugan@gmail.com>
2025-11-12 15:03:27 +0530

    fix web issue

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/ui/button"
+
 const HomePage = () => {
-  return <div>HomePage</div>
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h2>Starter Kit</h2>
+      <Button>Click Me</Button>
+    </div>
+  )
 }
 
 export default HomePage
